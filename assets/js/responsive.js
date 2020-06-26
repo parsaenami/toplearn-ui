4f2384daba9a30820a6da6790631b26a22fb1c8c
parsaenami <parsaenami@outlook.com>
2020-06-27 00:59:25 +0430

    added footer and did some responsive stuff

---
+++ assets/js/responsive.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    alert(1);
+    $("#xx").toggleClass("flex-row");
+    $("#xx").toggleClass("flex-column");
+  }
+});
