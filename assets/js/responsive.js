606c92480e87c9aa86c763677fcba901220d0894
parsaenami <parsaenami@outlook.com>
2020-07-02 16:26:18 +0430

    added blog posts

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
-  if (window.matchMedia("(max-width: 768px)").matches) {
-    alert(1);
-    $("#xx").toggleClass("flex-row");
-    $("#xx").toggleClass("flex-column");
-  }
-});
+// $(document).ready(function () {
+//   if (window.matchMedia("(max-width: 768px)").matches) {
+//     alert(1);
+//     $("#xx").toggleClass("flex-row");
+//     $("#xx").toggleClass("flex-column");
+//   }
+// });
